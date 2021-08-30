864a38c994047b3d91933f9276f1522a15b6ec51
risingstar2018 <33366277+risingstar2018@users.noreply.github.com>
2021-08-30 10:43:46 +0800

    fix: update SDK for fantom network

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -59,10 +59,10 @@ export function currencyEquals(currencyA: Currency, currencyB: Currency): boolea
 export const WETH = {
   [ChainId.MAINNET]: new Token(
     ChainId.MAINNET,
-    '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83',
     18,
-    'WBNB',
-    'Wrapped BNB'
+    'wFTM',
+    'Wrapped Fantom'
   ),
-  [ChainId.TESTNET]: new Token(ChainId.TESTNET, '0xaE8E19eFB41e7b96815649A6a60785e1fbA84C1e', 18, 'WBNB', 'Wrapped BNB')
+  [ChainId.TESTNET]: new Token(ChainId.TESTNET, '0x1b8733Bc06258e6201A56b080EA20D7BD4607d91', 18, 'wFTM', 'Wrapped Fantom')
 }
